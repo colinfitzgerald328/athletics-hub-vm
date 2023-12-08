d7dbfef2c6d764e363bf513c88c7f7ef864debb0
EC2 Default User <ec2-user@ip-172-31-38-152.ec2.internal>
2023-12-08 16:09:08 +0000

    add latest

--- processor/top_competitors_specific.py
+++ processor/top_competitors_specific.py
@@ -309,14 +309,81 @@ def return_results_dict(compiled_results):
         })
     return results_dict
 
+def get_timestamp(date: str) -> float: 
+  """
+  Returns a timestamp from a date string.
+  """
+  date_format = "%d %b %Y"
+  timestamp = datetime.strptime(date, date_format).timestamp()
+  return timestamp
+
+
+
+def return_results_desc(json_list: list) -> list:
+  """
+  Returns a list of results sorted by date in descending order.
+  """
+  for item in json_list:
+    date_string = item["date"]
+    timestamp = get_timestamp(date_string)
+    item["timestamp"] = timestamp
+  return sorted(json_list, key=lambda x: x['timestamp'], reverse=True)
+
+
+def get_athlete_results(athlete_id: str) -> list:
+  """
+  Returns a list of results for a given athlete.
+  """
+  headers = {
+      'Content-Type': 'application/json',
+      'Accept': '*/*',
+      'Sec-Fetch-Site': 'cross-site',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Sec-Fetch-Mode': 'cors',
+      # 'Accept-Encoding': 'gzip, deflate, br',
+      'Origin': 'https://worldathletics.org',
+      'User-Agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15',
+      'Referer': 'https://worldathletics.org/',
+      # 'Content-Length': '800',
+      'Connection': 'keep-alive',
+      'Host': 'wpgiegzkbrhj5mlsdxnipboepm.appsync-api.eu-west-1.amazonaws.com',
+      'Sec-Fetch-Dest': 'empty',
+      'x-amz-user-agent': 'aws-amplify/3.0.2',
+      'x-api-key': 'da2-juounigq4vhkvg5ac47mezxqge',
+  }
+
+  json_data = {
+      'operationName':
+      'GetSingleCompetitorResultsDate',
+      'variables': {
+          'resultsByYearOrderBy': 'date',
+          'id': int(athlete_id),
+      },
+      'query':
+      'query GetSingleCompetitorResultsDate($id: Int, $resultsByYearOrderBy: String, $resultsByYear: Int) {\n  getSingleCompetitorResultsDate(id: $id, resultsByYear: $resultsByYear, resultsByYearOrderBy: $resultsByYearOrderBy) {\n    parameters {\n      resultsByYear\n      resultsByYearOrderBy\n      __typename\n    }\n    activeYears\n    resultsByDate {\n      date\n      competition\n      venue\n      indoor\n      disciplineCode\n      disciplineNameUrlSlug\n      typeNameUrlSlug\n      discipline\n      country\n      category\n      race\n      place\n      mark\n      wind\n      notLegal\n      resultScore\n      remark\n      __typename\n    }\n    __typename\n  }\n}\n',
+  }
+
+  response = requests.post(
+      'https://wpgiegzkbrhj5mlsdxnipboepm.appsync-api.eu-west-1.amazonaws.com/graphql',
+      headers=headers,
+      json=json_data,
+  )
+
+  json_resp = response.json(
+  )["data"]["getSingleCompetitorResultsDate"]["resultsByDate"]
+
+  sorted_results = return_results_desc(json_resp)
+  return sorted_results
+
 
 def get_top_competitors(profile): 
     time.sleep(10)
-    response_data = requests.get(f"https://python.colinfitzgeral8.repl.co/athletes/results?athlete_id={profile['aaAthleteId']}").json()
-    results = response_data["athlete_data"]
+    athlete_results = get_athlete_results(profile["aaAthleteId"])
+
     name = profile["givenName"] + " " + profile["familyName"]
 
-    compiled_results = get_compiled_results(profile["gender"] + "'s", results)
+    compiled_results = get_compiled_results(profile["gender"] + "'s", athlete_results)
 
     results_dict = return_results_dict(compiled_results)
 
