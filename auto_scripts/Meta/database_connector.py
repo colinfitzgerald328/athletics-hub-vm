9cdd999b6cdefb67242c346499cfc102c664ec37
Colin <colinfitzgerald328@gmail.com>
2024-02-14 07:51:15 -0800

    issue a fix

---
+++ auto_scripts/Meta/database_connector.py
@@ -0,0 +1,25 @@
+from Meta.app_secrets import (
+    DATABASE_PASSWORD,
+    DATABASE_NAME,
+    COLLECTION_NAME,
+    DATABASE_USERNAME,
+)
+import pymongo
+
+
+def connect_to_db() -> pymongo.MongoClient:
+    client = pymongo.MongoClient(
+        f"mongodb+srv://{DATABASE_USERNAME}:"
+        + DATABASE_PASSWORD
+        + f"@{DATABASE_NAME}.tqfgaze.mongodb.net/?retryWrites=true&w=majority"
+    )
+    return client
+
+
+def get_collection(
+    collection_name: str = COLLECTION_NAME,
+) -> pymongo.collection.Collection:
+    client = connect_to_db()
+    database = client.get_database(DATABASE_NAME)
+    collection = database.get_collection(collection_name)
+    return collection
