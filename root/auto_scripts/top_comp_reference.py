5321722a05a238b7c914c1652ed0c563255089a3
Colin <colinfitzgerald328@gmail.com>
2024-02-18 09:57:23 -0800

    fix

--- root/auto_scripts/top_comp_reference.py
+++ root/auto_scripts/top_comp_reference.py
@@ -3,35 +3,34 @@ from ..meta.database_connector import DatabaseConnector
 
 collection = DatabaseConnector().get_collection()
 
-documents = collection.find({"$expr": {"$not": ["$top_competitors_with_reference"]}})
+documents = collection.find({"$expr": "$top_competitors"})
 
 for document in tqdm(documents):
     top_competitors_with_reference = []
     top_competitors = document["top_competitors"]
-    if top_competitors:
-        if len(top_competitors) > 0:
-            for competitor in top_competitors:
-                found_reference = None
-                projection = {"_id": 0}
-                cur = collection.find(
-                    {
-                        "$expr": {
-                            "$eq": [
-                                {"$concat": ["$givenName", " ", "$familyName"]},
-                                competitor,
-                            ]
-                        }
-                    },
-                    projection,
+    if top_competitors and len(top_competitors) > 0:
+        for competitor in top_competitors:
+            found_reference = None
+            projection = {"_id": 0}
+            cur = collection.find(
+                {
+                    "$expr": {
+                        "$eq": [
+                            {"$concat": ["$givenName", " ", "$familyName"]},
+                            competitor,
+                        ]
+                    }
+                },
+                projection,
+            )
+            cursor_results = list(cur)
+
+            if cursor_results:
+                found_reference = cursor_results[0]["aaAthleteId"]
+
+                top_competitors_with_reference.append(
+                    {"athlete_name": competitor, "athlete_id": found_reference}
                 )
-                cursor_results = list(cur)
-
-                if cursor_results:
-                    found_reference = cursor_results[0]["aaAthleteId"]
-
-                    top_competitors_with_reference.append(
-                        {"athlete_name": competitor, "athlete_id": found_reference}
-                    )
 
     collection.update_one(
         {"_id": document["_id"]},
